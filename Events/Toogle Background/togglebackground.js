b5aa6a99c8fb8d324317e7019916c76d95b821c2
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-02-06 20:37:29 +0100

    Toogle Background task finished

---
+++ Events/Toogle Background/togglebackground.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function changeColor() {
+    var bodyNode = document.querySelector('body');
+    bodyNode.classList.add('color');
+};
+
+function removeColor() {
+    var bodyNode = document.querySelector('body');
+    bodyNode.classList.remove('color');
+};
+
+
