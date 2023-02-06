b7b8cf3442bf0dc075bfec0ad15ae1dce548455a
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-02-06 19:39:46 +0100

    Soccer Player update

--- Events/Soccer Player/soccerplayer.js
+++ Events/Soccer Player/soccerplayer.js
@@ -1,33 +1,24 @@
-// var body = document.querySelector('body');
-// var button = document.getElementById('button');
-// var player = document.getElementById('img');
+var container = document.querySelector('.container');
+var button = document.getElementById('.button');
+var target = document.getElementById('.target');
 
 
-var target = document.getElementById('target');
+/*var target = document.getElementById('target');
 var targetBox = document.getElementById('target-box');
-targetBox.addEventListener('click', mouseClicked);
-function mouseClicked (event){
-  var xposition = (event.clientX - target.offsetLeft - target.offsetWidth/2);
-  var yposition = (event.clientY - target.offsetTop - target.offsetHeight/2);
-  target.style.transform = "translate("+ xposition + "px," + yposition + "px)";
-  console.log(event);
+targetBox.addEventListener('click', mouseClicked);*/
+var action = function () {
+  var x = event.clientX;
+  var y = event.clientY;
+  target.style.position = 'absolute';
+  target.style.left = x + 'px';
+  target.style.top = y + 'px';
 }
 
+  /*target.style.transform = "translate("+ xposition + "px," + yposition + "px)";
+  console.log(event);*/
 
-button.addEventListener('click', mouseClicked) {
-    document.getElementById('target');
-}
+container.addEventListener('click', move);
 
-
-
-
-/*
-.back-to-top
-
-
-function mouseClicked (event){
-    var xposition = (event.clientX - target.offsetLeft - target.offsetWidth/2);
-    var yposition = (event.clientY - target.offsetTop - target.offsetHeight/2);
-    target.style.transform = "translate("+ xposition + "px," + yposition + "px)";
-  console.log(event);
-} */
+button.addEventListener('click', function() {
+    container.removeEventListener('click', move);
+});
